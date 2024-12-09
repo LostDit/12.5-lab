c4cd2f06e518240f5a85c97a258ce97cf61f23d7
@MaKc@ @MaKc@ <maksim.antonov2012@yandex.ru>
2024-12-09 14:47:11 +0300

    Первое задание вроде сделал

---
+++ Многоквартирный дом/Многоквартирный дом.cpp
@@ -0,0 +1,22 @@
+﻿//Используется массив строк для хранения данных.
+//В коде не используются библиотеки, кроме <iostream> и <string>.
+//Программа проверяет доступность элемента по индексу и выдаёт корректный результат.
+#include <iostream>
+#include <string>
+
+int main() {
+
+    std::string sitizen[10] = {}; int j;
+    for (int i = 0; i < 10; i++) {
+        getline(std::cin, sitizen[i]);
+    }
+    
+    std::cout << "Enter number: ";
+    std::cin >> j;
+
+    if (j <= 0 || j > 10) {
+        std::cout << "Out of range" << "\n";
+    }else std::cout << sitizen[j - 1] << "\n";
+    
+    return 0;
+}

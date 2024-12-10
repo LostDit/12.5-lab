d1127afbf0e09b15240307bdcf9bc999d2094c12
@MaKc@ @MaKc@ <maksim.antonov2012@yandex.ru>
2024-12-10 15:20:55 +0300

    Вроде ок всё

---
+++ Сортировка/Сортировка.cpp
@@ -0,0 +1,35 @@
+﻿//Используется массив чисел с плавающей точкой для хранения данных.
+//В коде не используются библиотеки, кроме <iostream>.
+//Программа корректно использует обращения по индексам и выдаёт правильный ответ.
+//Асимптотическая сложность алгоритма не более O(n ^ 2).
+//Числа выводятся от большего к меньшему.
+#include <iostream>
+
+int main() {
+    float array[15], tempvar1, tempvar2; bool chek = true;
+    for (int i = 0; i < 15; i++) {
+        std::cin >> array[i];
+    }
+    while (chek) {
+        int correct = 0;
+        for (int i = 0; i < 14; i++) {
+            if (array[i] < array[i + 1]) {
+                tempvar2 = array[i + 1];
+                tempvar1 = array[i];
+                array[i + 1] = tempvar1;
+                array[i] = tempvar2;
+            }
+        }
+        for (int i = 0; i < 14; i++) {
+            if (array[i] > array[i + 1]) {
+                correct++;
+            }
+            if (correct == 14) {
+                chek = false;
+            }
+        }
+    }
+    for (int i = 0; i < 15; i++) {
+        std::cout << array[i] << " ";
+    }
+}
